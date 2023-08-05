3740ed99e0e4f6a9adaed89ec132e70175715c12
Derek Day <day.derek7@protonmail.com>
2023-08-04 21:34:27 -0500

    getting started, create-next-app, shadcn

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div className="p-4">
+        <Button variant="outline">Test</Button>
+      </div>
+    </>
+  );
+}
