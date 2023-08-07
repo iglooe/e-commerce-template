41ce321351a91e55346088bdc62fc73636fcab67
Derek Day <day.derek7@protonmail.com>
2023-08-06 22:42:32 -0500

    data table, api get, fetch, and delete work now

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,34 @@
+import { format } from "date-fns";
+import prismadb from "@/lib/prismadb";
 import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
 
-const BillboardsPage = () => {
+const BillboardsPage = async ({
+  params,
+}: {
+  params: {
+    storeId: string;
+  };
+}) => {
+  const billboard = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedBillboards: BillboardColumn[] = billboard.map((b) => ({
+    id: b.id,
+    label: b.label,
+    createdAt: format(b.createdAt, "MMMM do, yyyy"),
+  }));
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );
